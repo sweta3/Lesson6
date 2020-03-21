22ca635bd727e67ed75a47a162c56906ac3a5fdf
sweta3 <Svetlana.Maskajeva@gmail.com>
2020-03-21 10:36:02 +0200

    adding new lesson6 java fules

--- Lesson6/src/com/lesson6/SwitchDays.java
+++ Lesson6/src/com/lesson6/SwitchDays.java
@@ -16,18 +16,20 @@ public class SwitchDays {
 	
    public static void main(String args[]) {
        // declare a variable to store the day entered by the user
-	   String day;
+	   int day;
+//	   String day;
        // declare a variable named scan to be able to work with objects of type Scanner
 	   // create a Scanner object to read input from the keyboard
 	   Scanner input = new Scanner (System.in);
        // prompt the user to enter a day of the week
-	   System.out.println("Please enter a day of the week to see whether there are any classes scheduled for that day");
+	   System.out.println("Please enter a day (as a number i.e. 1-7) of the week to see whether there are any classes scheduled for that day");
        // read the string provided by the user
-	   day=input.next();
+	  // day=input.next();
+	   day=input.nextInt();
        // we know that Java is case sensitive, therefore let's convert the word provided by the user to lower case so that the program 
 	   //works independent of the way the user writes the word e.g. Monday, monDAY, MoNdAy
        // toLowerCase() is a method declared in the class String. The method converts all the characters in a given string to lower case
-	   day=day.toLowerCase();
+//	   day=day.toLowerCase();
        //switch () {
 //note: a switch statement accepts a constant expression/variable of type String only starting with Java version 7; in earlier versions the compiler shows an "incompatible types" error.
            //case "monday":
@@ -35,24 +37,45 @@ public class SwitchDays {
                //break;
        //}
 	   switch(day){
-	   case "monday":
-	   case "wednesday":
+	   case 1:
+	   case 3:
 		   System.out.println("Evening classes at 19:00");
 		   break;
-	   case "tuesday":
-	   case "thursday":
-	   case "friday":
+	   case 2:
+	   case 4:
+	   case 5:
 		   System.out.println("no classes");
 		   break;
-	   case "saturday":
+	   case 6:
 		   System.out.println("Whole day labs :)");
 		   break;
-	   case "sunday":
+	   case 7:
 		   System.out.println("It's finally Sunday!");
 		   break;
 	   default:   
 		   System.out.println("Check your input mate/fella/gal");
 		   break;
 	   }
+     
+//	   switch(day){
+//	   case "monday":
+//	   case "wednesday":
+//		   System.out.println("Evening classes at 19:00");
+//		   break;
+//	   case "tuesday":
+//	   case "thursday":
+//	   case "friday":
+//		   System.out.println("no classes");
+//		   break;
+//	   case "saturday":
+//		   System.out.println("Whole day labs :)");
+//		   break;
+//	   case "sunday":
+//		   System.out.println("It's finally Sunday!");
+//		   break;
+//	   default:   
+//		   System.out.println("Check your input mate/fella/gal");
+//		   break;
+//	   }
    }
 }
